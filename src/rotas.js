7546b95373e806ac0653afeb992eaee13c1b6e8d
Brenna-silvaa <brennahfs12@hotmail.com>
2021-11-27 17:30:43 -0300

    cadastro e login usuarios

--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const usuarios = require('./controladores/usuarios');
+const login = require('./controladores/login');
 
 const rotas = express();
 
 rotas.post('/cadastro', usuarios.cadastrarUsuarios);
-rotas.post('login');
+rotas.post('/login', login.login);
 
 
 module.exports = rotas
