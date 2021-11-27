b81ef803d75e75d1bcbd62a94d6e16c2ad3b93f5
Quezia <queziaamenezes@gmail.com>
2021-11-27 17:27:26 -0300

    busca de serviços geral e por categoria

--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const usuarios = require('./controladores/usuarios');
+const servicos = require('./controladores/serviços');
 
 const rotas = express();
 
 rotas.post('/cadastro', usuarios.cadastrarUsuarios);
 rotas.post('login');
 
+rotas.get('/servicos', servicos.buscarServiços);
+rotas.get('/servicos/:categoria', servicos.buscaPorCategoria);
 
-module.exports = rotas
+module.exports = rotas;
