34d383fc34d94235e9d256ca7b34c8b8c124ac8e
Jéssica Zanchetta <jessica.zanchetta@hotmail.com>
2021-11-27 22:06:03 -0300

    Configuraçao banco de dados

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,13 @@
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'hackaton_provi',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+      rejectUnauthorized: process.env.DB_SSL === 'false' ? false : true,
+    },
   },
 })
 
